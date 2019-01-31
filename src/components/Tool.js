239e2c97c3f06ab28f2da9e3315ac3414fcf964f
fortunexfortune <osadolor.fortune@yahoo.it>
2019-01-31 09:07:45 +0000

    added note inforamtion in the tool page

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -13,23 +13,20 @@ class Tool extends Component {
         if(JSON.parse(sessionStorage.getItem("Account")) === null){
             return(<h3 className="container center-align" > This is a Restricted Page, you must log in to access this page content</h3>)
          }
-        console.log(this.state.logged_user);
-        console.log(JSON.stringify(this.state.logged_user));
+        // console.log(this.state.logged_user);
+        // console.log(JSON.stringify(this.state.logged_user));
         return (
                 <div className="col s7" className="container">
-                     {/* <h4>Username : {this.props.logged_user.userName}</h4>
-                    <br></br>
-                    {(this.props.logged_user.notes.map((note) =>
+                     {(this.state.logged_user.notes.reverse().map((note) =>
                         <div className="post card" key={note.noteID}>
                             <div className="card-content" >
                                 <span className="card-title">  <b> Title : {note.title} </b></span>
                                 <span className="card-title"> <em>ID</em> : {note.noteID} <b /> </span>
-
                                 <br /> date :  {note.date}
                                 <br /> content :{note.content}
                             </div>
                         </div>)
-                    )} */}
+                    )}
                 </div>
         )
     }
