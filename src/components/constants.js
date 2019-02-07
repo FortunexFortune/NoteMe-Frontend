60d2a425fb06ed72b0b9a71ccaef5e2937fe3286
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-07 12:55:00 +0000

    switched back to static ip

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
-export const static_IP = "http://localhost";  
-// export const static_IP = "http://35.242.146.143"; 
+// export const static_IP = "http://localhost";  
+export const static_IP = "http://35.242.146.143"; 
