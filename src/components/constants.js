2c8fdebcf77ba325919f04522d0504ebedd5dba2
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-08 10:18:47 +0000

    switched back to static ip adress

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,17 +1,11 @@
-// export const static_IP = "http://35.242.146.143"; 
-export const static_IP = "http://localhost";  
-
-
-
+export const static_IP = "http://35.242.146.143"; 
+// export const static_IP = "http://localhost";  
 
 
 export const getAllAccounts = ":8080/SpeedMe_Backend/api/account/getAllAccounts";  
 export const getAccount = ":8080/SpeedMe_Backend/api/account/getAccount/";  
 export const deleteAccount = ":8080/SpeedMe_Backend/api/account/deleteAccount/";  
 export const updateAccount = ":8080/SpeedMe_Backend/api/account/updateAccount/";  
-
-
-
 
 
 export const getAllNotes = ":8080/SpeedMe_Backend/api/note/getAllNotes";  
