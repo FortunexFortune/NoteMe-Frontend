ef8956737c89bade2dd6005b69b433b1477e7c54
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-08 10:16:04 +0000

    implemented new constants

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,20 @@
-export const static_IP = "http://localhost";  
 // export const static_IP = "http://35.242.146.143"; 
+export const static_IP = "http://localhost";  
+
+
+
+
+
+export const getAllAccounts = ":8080/SpeedMe_Backend/api/account/getAllAccounts";  
+export const getAccount = ":8080/SpeedMe_Backend/api/account/getAccount/";  
+export const deleteAccount = ":8080/SpeedMe_Backend/api/account/deleteAccount/";  
+export const updateAccount = ":8080/SpeedMe_Backend/api/account/updateAccount/";  
+
+
+
+
+
+export const getAllNotes = ":8080/SpeedMe_Backend/api/note/getAllNotes";  
+export const createNote = ":8080/SpeedMe_Backend/api/note/createNote";
+export const deleteNote = ":8080/SpeedMe_Backend/api/note/deleteNote/";  
+export const updateNote = ":8080/SpeedMe_Backend/api/note/updateNote/";  
