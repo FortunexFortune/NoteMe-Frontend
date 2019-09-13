51050e44775c7671e695a3800b227544a1e71487
Fortune Osadolor <33355125+FortunexFortune@users.noreply.github.com>
2019-09-13 10:12:08 +0100

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
 
 const constants = {
-static_IP : "http://35.242.146.143"
-// static_IP : "http://localhost"
+//static_IP : "http://35.242.146.143"
+static_IP : "http://localhost"
 }
 
 module.exports = constants
